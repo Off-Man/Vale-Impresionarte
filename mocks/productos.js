c0b33321f0b909b51be4d767000840a7ae46063f
Dario Manuel <dariomanuel999@gmail.com>
2024-03-06 16:33:25 -0300

    with errors

---
+++ mocks/productos.js
@@ -0,0 +1,115 @@
+export const productos = [
+    {
+        id: 1,
+        name: "ADOS-2 COMPLETO",
+        price: 1200,
+        category: "Pruebas Psicometricas",
+        img: "../img/productos/small/ados-2-completo.jpg"
+    },
+    {
+        id: 2,
+        name: "PLS 5",
+        price: 1200,
+        category: "Pruebas Psicometricas",
+        img: "../img/productos/small/pls-5.jpg"
+    },
+    {
+        id: 3,
+        name: "TELD 3",
+        price: 1200,
+        category: "Pruebas Psicometricas",
+        img: "../img/productos/small/teld-3.jpg"
+    },
+    {
+        id: 4,
+        name: "BEI. BATERÍA DE EVALUACIÓN INFANTIL",
+        price: 1200,
+        category: "Pruebas Psicometricas",
+        img: "../img/productos/small/bei-bateria-de-eval-infantil.jpg"
+    },
+    {
+        id: 5,
+        name: "ABECEDARIO",
+        price: 1200,
+        category: "Material para consultorio",
+        img: "../img/productos/small/abecedario-1.jpeg"
+    },
+    {
+        id: 6,
+        name: "ABECEDARIO",
+        price: 1200,
+        category: "Material para consultorio",
+        img: "../img/productos/small/abecedario-2.jpeg"
+    },
+    {
+        id: 7,
+        name: "ABECEDARIO",
+        price: 1200,
+        category: "Material para consultorio",
+        img: "../img/productos/small/abecedario-3.jpeg"
+    },
+    {
+        id: 8,
+        name: "ABECEDARIO",
+        price: 1200,
+        category: "Material para consultorio",
+        img: "../img/productos/small/abecedario-4.jpeg"
+    },
+    {
+        id: 9,
+        name: "MATERIALES CONCRETOS ENI 2",
+        price: 1200,
+        category: "Materiales concretos",
+        img: "../img/productos/small/m-concreto-eni-2.jpg"
+    },
+    {
+        id: 10,
+        name: "ANILLAS ENFEN DE PVC",
+        price: 1200,
+        category: "Materiales concretos",
+        img: "../img/productos/small/anillas-enfen-pvc.jpg"
+    },
+    {
+        id: 11,
+        name: "ESTIMULO DEL WISC IV",
+        price: 1200,
+        category: "Materiales concretos",
+        img: "../img/productos/small/estimulo-del-wisc-iv.jpg"
+    },
+    {
+        id: 12,
+        name: "TARJETAS DE LOCALIZACION Y PLANOS",
+        price: 1200,
+        category: "Materiales concretos",
+        img: "../img/productos/small/tarjetas-localiz-y-planos.jpeg"
+    },
+    {
+        id: 13,
+        name: "CUADERNO DE REGISTRO DE SESIÓN",
+        price: 1200,
+        category: "Agendas y cuadernos",
+        img: "../img/productos/small/cuaderno-registro-sesion.jpg"
+    },
+    {
+        id: 14,
+        name: "CUADERNOS A5. RAYADOS",
+        price: 1200,
+        category: "Agendas y cuadernos",
+        img: "../img/productos/small/cuaderno-a5-rayados.jpg"
+    },
+    {
+        id: 15,
+        name: "AGENDA 2024 DIARIA",
+        price: 1200,
+        category: "Agendas y cuadernos",
+        img: "../img/productos/small/agenda-diaria-2024.jpeg"
+    },
+    {
+        id: 16,
+        name: "AGENDA PSICO DOS DIAS POR PAGINA",
+        price: 1200,
+        category: "Agendas y cuadernos",
+        img: "../img/productos/small/agenda-psico-dos-dias-por-pagina.jpeg"
+    },
+]
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./js/**/*.{html,js}",
+        "./mocks/**/*.{html,js}",
+        "./index.html",
+    
+    ],
+    
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
